2dab3cc1efb6e478acba85e1859c60553a9fc187
namelessvoid <simon_kerler@web.de>
2018-03-18 20:18:33 +0100

    Use mcmaster cli parsing

--- ClassGenerator/ClassMetaInfo.cs
+++ ClassGenerator/ClassMetaInfo.cs
@@ -5,41 +5,19 @@ namespace ClassGenerator
 {
     public class ClassMetaInfo
     {
-        public static ClassMetaInfo FromDefinition(string definition)
-        {
-            Console.WriteLine($"Constructing class from definition: \"{definition}\".");
-
-            var matches = Regex.Matches(definition, @"^(?<location>[\w/]+?):(?<namespace>[\w]+?)::(?<class>[\w]+?)$");
-
-            if(matches.Count != 1)
-            {
-                Console.WriteLine("Malformed definition. See --help for further information.");
-                return null;
-            }
-
-            var components = matches[0];
-            var location = components.Groups["location"].Value;
-            var namespaceName = components.Groups["namespace"].Value;
-            var className = components.Groups["class"].Value;
-
-            return new ClassMetaInfo
-            {
-                Location = location,
-                ClassName = className,
-                Namespace = namespaceName
-            };
-        }
-
         // The path at which the class is placed.
         // NOT prepended with 'include/' or 'src/'.
         public string Location { get; private set; }
 
+        public string Namespace { get; private set; }
         public string ClassName { get; private set; }
 
-        public string Namespace { get; private set; }
-
         // Hide constructor
-        private ClassMetaInfo()
-        {}
+        public ClassMetaInfo(string location, string className, string @namespace)
+        {
+            Location = location;
+            ClassName = className;
+            Namespace = @namespace;
+        }
     }
 }
--- ClassGenerator/Program.cs
+++ ClassGenerator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using McMaster.Extensions.CommandLineUtils;
 
 namespace ClassGenerator
 {
@@ -7,43 +8,52 @@ namespace ClassGenerator
     {
         static int Main(string[] args)
         {
-            if(!CheckArgs(args))
+            var app = new CommandLineApplication();
+            app.Name = "classgen";
+            app.HelpOption("-?|-h|--help");
+
+            app.Command("class", (command) =>
             {
-            	return 1;
-            }
+                command.Description = "Generate a full class, i.e. header and source file.";
+                command.HelpOption("-?|-h|--help");
 
-            var classMetaInfo = ClassMetaInfo.FromDefinition(args[0]);
+                var locationOption = command
+                    .Option("-l|--location", "The folder in which the class is put, NOT prepended with 'include/' or 'src/'.", CommandOptionType.SingleValue)
+                    .IsRequired();
 
-            if(classMetaInfo == null)
+                var namespaceOption = command
+                    .Option("-n|--namespace", "The namespace of the class.", CommandOptionType.SingleValue)
+                    .IsRequired();
+
+                var classNameOption = command
+                    .Option("-c|--classname", "The name of the class.", CommandOptionType.SingleValue)
+                    .IsRequired();
+
+                command.OnExecute(() =>
+                {
+                    var classMetaInfo = new ClassMetaInfo(
+                        locationOption.Value(),
+                        classNameOption.Value(),
+                        namespaceOption.Value()
+                    );
+
+                    var filesystem = new Filesystem();
+                    var classGenerator = new ClassGenerator(filesystem);
+                    classGenerator.GenerateClass(classMetaInfo);
+
+                    return 0;
+                });
+            });
+
+            app.OnExecute(() =>
             {
-            	return 1;
-            }
+                Console.WriteLine("Specify a command");
+                app.ShowHelp();
+                return 1;
+            });
 
-            var filesystem = new Filesystem();
-            var classGenerator = new ClassGenerator(filesystem);
-            classGenerator.GenerateClass(classMetaInfo);
-
-            return 0;
+            Console.WriteLine("This is C++ Class Generator");
+            return app.Execute(args);
         }
-
-        static bool CheckArgs(string[] args)
-        {
-        	if(args.Length != 1 || args[0].Equals("--help") || args[0].Equals("-h"))
-        	{
-        		Console.WriteLine(HelpMessage);
-        		return false;
-        	}
-
-        	return true;
-        }
-
-        private static string HelpMessage =
-        	"C++ header generate tool\n" +
-        	"------------------------\n\n" +
-        	"Usage:\n" +
-        	$" {System.AppDomain.CurrentDomain.FriendlyName} <location:namespace::classname>\n\n" +
-        	"Example:\n" +
-        	$" {System.AppDomain.CurrentDomain.FriendlyName} \"game/states:qrw::SkirmishState\""
-        	;
-	}
+    }
 }
